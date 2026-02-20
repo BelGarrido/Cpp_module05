816edb9ad11c6badd41d89d1a6bfa15e542ec981
Ana Garrido Diaz <anagarri@c3r8s2.42malaga.com>
2026-02-20 11:26:54 +0100

    RobotomyRequest started

--- ex02/Form.hpp
+++ ex02/Form.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <iostream>
+#include <fstream>
 #include "colors.hpp"
 //#include "Bureaucrat.hpp"
 
--- ex02/RobotomyRequestForm.hpp
+++ ex02/RobotomyRequestForm.hpp
@@ -1,28 +1,25 @@
-#ifndef SHRUBBERY_HPP
-#define SHRUBBERY_HPP
+#ifndef ROBOTOMY_HPP
+#define ROBOTOMY_HPP
 
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
-#include <iostream>
-#include <fstream>
 
-class ShrubberyCreation: public AForm {
+class RobotomyRequest: public AForm {
     private:
         std::string _target;
 
     public:
-        ShrubberyCreation();
-        ShrubberyCreation(std::string &target);
-        ShrubberyCreation(const ShrubberyCreation &original);
+        RobotomyRequest();
+        RobotomyRequest(std::string &target);
+        RobotomyRequest(const RobotomyRequest &original);
         //no se hacerlo, se me ocurre llamar al constructor de Aform en el cosntructor de SC y hay settear los artibutos (?)
         
         //tengo que checkear las copias
-        ShrubberyCreation &operator=(const ShrubberyCreation &original);
-        ~ShrubberyCreation();
+        RobotomyRequest &operator=(const RobotomyRequest &original);
+        ~RobotomyRequest();
 
         virtual bool beSigned(Bureaucrat &b);
         virtual bool beExecuted(Bureaucrat &b);
 } ;
-
 
 #endif
